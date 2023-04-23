164f8e837d998f0046de5434391438eccdfce711
stefano-mazziotta <mazziottastefano@gmail.com>
2023-04-23 17:48:18 -0300

    install graphql and initialize

---
+++ src/graphql/schema.ts
@@ -0,0 +1,16 @@
+export const typeDefs = `
+    type Link {
+        id: ID
+        title: String
+        description: String
+        url: String
+        category: String
+        imageUrl: String
+        users: [String]
+    }
+
+    type Query {
+        links: [Link]!
+    }
+
+`
