24842620a3432bb3073447d40bae99b695293f5b
stefano-mazziotta <mazziottastefano@gmail.com>
2023-04-23 17:12:44 -0300

    create models - seed database - install prisma studio

---
+++ data/links.js
@@ -0,0 +1,36 @@
+export const links = [
+    {
+        category: "Open Source",
+        description: "Fullstack React framework",
+        id: 1,
+        imageUrl: "https://res.cloudinary.com/practicaldev/image/fetch/s--KkP4AuFt--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/uploads/articles/ql92zpsre2li58mpjpsa.png",
+        title: "Next.js",
+        url: "https://nextjs.org",
+    },
+    {
+        category: "Open Source",
+        description: "Next Generation ORM for TypeScript and JavaScript",
+        id: 2,
+        imageUrl: "https://res.cloudinary.com/practicaldev/image/fetch/s--p7gPRYIN--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/3bzg30pauwbl828gcl84.png",
+
+        title: "Prisma",
+        url: "https://prisma.io",
+    },
+    {
+        category: "Open Source",
+        description: "Utility-fist css framework",
+        id: 3,
+        imageUrl:
+            "https://www.solucionex.com/sites/default/files/posts/imagen/tailwindcss-1633184775.jpeg",
+        title: "TailwindCSS",
+        url: "https://tailwindcss.com",
+    },
+    {
+        category: "Open Source",
+        description: "GraphQL implementation ",
+        id: 4,
+        imageUrl: "https://www.apollographql.com/apollo-home.jpg",
+        title: "Apollo GraphQL",
+        url: "https://apollographql.com",
+    },
+];
