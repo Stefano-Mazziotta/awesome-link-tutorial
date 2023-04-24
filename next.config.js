60a534390a91bf30ca464e514cd862d5c7ed95d1
stefano-mazziotta <mazziottastefano@gmail.com>
2023-04-23 22:22:03 -0300

    [layout] Sending requests using useQuery

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  images: {
+    domains: ['res.cloudinary.com','www.solucionex.com', 'www.apollographql.com'],
+  },
 }
 
 module.exports = nextConfig
---
+++ src/components/Layout/index.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+// import Header from "./Header";
+
+interface Props {
+  children: React.ReactNode
+}
+const Layout: React.FC<Props> = ({ children }) => {
+  return (
+    <div>
+      {children}
+    </div>
+  );
+};
+
+export default Layout;
